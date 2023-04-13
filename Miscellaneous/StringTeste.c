26b36f913c89ecc1a89eb19510f30d3f16901c22
Vladimyr Guedes <vladguedes@hotmail.com>
2023-04-13 13:03:42 -0300

    ...

---
+++ Miscellaneous/StringTeste.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char nome[] = "Aoba";
+
+    printf("Seu nome eh %s\n%c %c %c %c", nome, nome[0], nome[1], nome[2], nome[3]);
+
+    return(0);
+}
--- Miscellaneous/Teste.c
+++ Miscellaneous/Teste.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef enum boolean
+{
+    false,
+    true
+}Boolean;
+
 
 int main() {
-    printf("Hello World!");
+
     return(0);
 }
